08e65d445a3cf5fe5e7ab819540e89e261f7fbe6
unknown <jovannyjosef_rivera@outlook.com>
2025-01-28 16:21:11 -0600

    Crear un array para almacenar los nombres

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/*
+El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+Aquí deberás desarrollar la lógica para resolver el problema.
+
+Funciones principales que se tiene que implementar (Trello)
+1 - Crear un array para almacenar los nombres
+2 - Implementa una función para agregar amigos
+3 - Implementa una función para actualizar la lista de amigos
+4 - Implementa una función para sortear los amigos
+5 - Implemetar una función para mostrar el resultado del sorteo
+ */
+
+// ********** Array para almacenar los nombres **********
+
+let listaAmigos = [];
+
+//console.log(listaAmigos)
